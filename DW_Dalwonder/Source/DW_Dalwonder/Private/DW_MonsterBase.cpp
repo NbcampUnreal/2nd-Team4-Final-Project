ac5480be2800f2a340cbe6b47f258a6e235fb20b
Cheon1219 <j1219j1@naver.com>
2025-05-08 00:03:17 +0900

    Add: MonsterBase
    MonsterBase 일부 추가

---
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/DW_MonsterBase.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DW_MonsterBase.h"
+
+
+// Sets default values
+ADW_MonsterBase::ADW_MonsterBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADW_MonsterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADW_MonsterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ADW_MonsterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+EMonsterState ADW_MonsterBase::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void ADW_MonsterBase::SetCurrentState(EMonsterState MonsterState)
+{
+	CurrentState = MonsterState;
+}
+
