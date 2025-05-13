820cfeb9ba8c47e80769fcee8e661d5ea9ce25d9
Potato_Nim <alswo0103kk@naver.com>
2025-05-13 21:51:31 +0900

    [ Update ] Skeleton Knights anim montage update. 해골 기사 애님 몽타주 수정, 애님 노티파이 추가중.

--- DW_Dalwonder/Source/DW_Dalwonder/Private/Monster/NormalMonster/Skeleton/MobSkeleton.cpp
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/Monster/NormalMonster/Skeleton/MobSkeleton.cpp
@@ -2,4 +2,14 @@
 
 
 #include "Monster/NormalMonster/Skeleton/MobSkeleton.h"
+#include "Components/CapsuleComponent.h"
+
+AMobSkeleton::AMobSkeleton()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+
+	TraceStart->SetupAttachment(GetMesh(), TEXT("hand_r"));
+	TraceEnd->SetupAttachment(GetMesh(), TEXT("hand_r"));
+}
 
