665d03428784e45f0fa396fb46187c258c48f29b
nyamnyumnyeom <jincar000@naver.com>
2025-05-14 20:31:04 +0900

    [ add ] interact outline

--- DW_Dalwonder/Source/DW_Dalwonder/Private/DW_Smite.cpp
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/DW_Smite.cpp
@@ -11,6 +11,7 @@ void ADW_Smite::Interact_Implementation(AActor* Interactor)
 {
     Super::Interact_Implementation(Interactor);
 
+    //여기에 상호작용시 로직 작성
     if (!SmiteWidgetClass) return;
 
     if (ADW_GmBase* GM = Cast<ADW_GmBase>(UGameplayStatics::GetGameMode(this)))
