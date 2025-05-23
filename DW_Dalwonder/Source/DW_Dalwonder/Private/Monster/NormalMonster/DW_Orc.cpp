3bc596d227172dc63167070f49ff41ef887deb4e
cxvm <cxvmcxvm2@naver.com>
2025-05-23 15:51:24 +0900

    asd

---
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/Monster/NormalMonster/DW_Orc.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster/NormalMonster/DW_Orc.h"
+
+
+// Sets default values
+ADW_Orc::ADW_Orc()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComponent->SetupAttachment(GetMesh(), TEXT("hand_r")); 
+}
+
+// Called when the game starts or when spawned
+void ADW_Orc::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADW_Orc::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ADW_Orc::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/Monster/NormalMonster/DW_Orc.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DW_NormalMonsterBase.h"
+#include "DW_Orc.generated.h"
+
+UCLASS()
+class DW_DALWONDER_API ADW_Orc : public ADW_NormalMonsterBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ADW_Orc();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	UStaticMeshComponent* MeshComponent;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
