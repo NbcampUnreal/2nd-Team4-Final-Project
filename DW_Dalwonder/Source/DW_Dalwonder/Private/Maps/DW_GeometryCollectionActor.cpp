55a9c2c8bf665c24762ba111a2237658f79be82d
Cheon <j1219j1@naver.com>
2025-05-25 02:17:34 +0900

    [add] : ChaosDestruction
    ChaosDestruction 추가

---
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/Maps/DW_GeometryCollectionActor.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Maps/DW_GeometryCollectionActor.h"
+
+#include "GeometryCollection/GeometryCollectionComponent.h"
+
+
+// Sets default values
+ADW_GeometryCollectionActor::ADW_GeometryCollectionActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	if (UGeometryCollectionComponent* GCComponent = GetGeometryCollectionComponent())
+	{
+		GCComponent->SetSimulatePhysics(false);
+		GCComponent->SetEnableGravity(false);
+		GCComponent->SetCollisionProfileName("NoCollision");
+		GCComponent->SetCollisionObjectType(ECC_WorldDynamic);
+	}
+}
+
+void ADW_GeometryCollectionActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ADW_GeometryCollectionActor::DestructActor()
+{
+	if (UGeometryCollectionComponent* GCComponent = GetGeometryCollectionComponent())
+	{
+		GCComponent->SetEnableGravity(true); // 중력 다시 켜줌
+		GCComponent->SetSimulatePhysics(true);
+		GCComponent->SetNotifyRigidBodyCollision(true);
+		GCComponent->SetCollisionProfileName("OverlapOnlyPawn");
+		GCComponent->SetCollisionObjectType(ECC_WorldDynamic);
+
+		FVector const LaunchDirection = FVector(0.0f, 0.0f, -1.0f).GetSafeNormal();
+		float const ImpulseStrength = 1000.0f;
+
+		GCComponent->AddImpulse(LaunchDirection * ImpulseStrength, NAME_None, true);
+		// 일정 시간 후 제거
+		// GetWorldTimerManager().SetTimer(
+		// 	DestroyTimerHandle,
+		// 	this,
+		// 	&ADW_GeometryCollectionActor::DestroyActor,
+		// 	5.0f,
+		// 	false
+		// );
+	}
+}
+
+void ADW_GeometryCollectionActor::DestroyActor()
+{
+	Destroy();
+}
---
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/Maps/DW_GeometryCollectionActor.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GeometryCollection/GeometryCollectionActor.h"
+#include "DW_GeometryCollectionActor.generated.h"
+
+UCLASS()
+class DW_DALWONDER_API ADW_GeometryCollectionActor : public AGeometryCollectionActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADW_GeometryCollectionActor();
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void DestructActor();
+
+	UFUNCTION(BlueprintCallable)
+	void DestroyActor();
+
+	// FTimerHandle DestroyTimerHandle;
+};
