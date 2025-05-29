c511a6c91b6035323c471d89e530351ec15bce48
Menho91 <agh914@naver.com>
2025-05-29 17:00:29 +0900

    [fix] Fixed DamageType Constructor

--- DW_Dalwonder/Source/DW_Dalwonder/Private/DW_DamageType.cpp
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/DW_DamageType.cpp
@@ -3,3 +3,14 @@
 UDW_DamageType::UDW_DamageType()
 {
 }
+
+UDW_DamageType* UDW_DamageType::CreateDamageType(bool bCanKnockdown)
+{
+	UDW_DamageType* NewDamageType = NewObject<UDW_DamageType>();
+	if (IsValid(NewDamageType))
+	{
+		NewDamageType->bCanKnockdown = bCanKnockdown;
+	}
+
+	return NewDamageType;
+}
--- DW_Dalwonder/Source/DW_Dalwonder/Public/DW_DamageType.h
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/DW_DamageType.h
@@ -4,14 +4,19 @@
 #include "GameFramework/DamageType.h"
 #include "DW_DamageType.generated.h"
 
+// 대미지 타입 생성은 UDW_DamageType::CreateDamageType(bool bCanKnockdown) 으로 생성
 UCLASS()
 class DW_DALWONDER_API UDW_DamageType : public UDamageType
 {
 	GENERATED_BODY()
 
 public:
-	UDW_DamageType();
+	// 공격이 Knockdown 시킬 수 있는 지 bCanKnockdown 으로 판별
+	static UDW_DamageType* CreateDamageType(bool bCanKnockdown);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DamageType")
 	bool bCanKnockdown = false;
+
+private:
+	UDW_DamageType();
 };
