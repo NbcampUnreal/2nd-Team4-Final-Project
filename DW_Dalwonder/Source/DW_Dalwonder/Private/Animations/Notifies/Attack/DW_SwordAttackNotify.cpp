561306f05fa68ed53065a6b9a369ba80287b2465
Cheon <j1219j1@naver.com>
2025-06-02 15:44:51 +0900

    [fix] bug

--- DW_Dalwonder/Source/DW_Dalwonder/Private/Animations/Notifies/Attack/DW_SwordAttackNotify.cpp
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/Animations/Notifies/Attack/DW_SwordAttackNotify.cpp
@@ -3,7 +3,6 @@
 #include "Animations/Notifies/Attack/DW_SwordAttackNotify.h"
 #include "DrawDebugHelpers.h"
 #include "Character/DW_Warrior.h"
-#include "Character/DW_Sword.h"
 #include "Interface/BearableInterface.h"
 #include "Character/DW_SwordBase.h"
 #include "Kismet/GameplayStatics.h"
