a9597d89d2dca5691bc563f63b19ce22decf19d5
Roanne <83761585+Roanne0258@users.noreply.github.com>
2025-06-07 16:35:32 +0900

    fix: BossHUDWidget
    보스 최대 체력 바인딩하고 정상작동 확인 완료

--- DW_Dalwonder/Source/DW_Dalwonder/Private/UI/Widget/BossHUDWidget.cpp
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/UI/Widget/BossHUDWidget.cpp
@@ -7,12 +7,18 @@
 
 void UBossHUDWidget::InitBossHUD(const FString& BossName, float MaxHP)
 {
+	MaxBossHP = MaxHP;
+	
 	if (BossNameText)
 	{
 		BossNameText->SetText(FText::FromString(BossName));
 	}
 
-	MaxBossHP = MaxHP;
+	if (BossHPText)
+	{
+		FString Text = FString::Printf(TEXT("%.0f"), MaxBossHP);
+		BossHPText->SetText(FText::FromString(Text));
+	}
 
 	if (BossHPBar)
 	{
@@ -22,8 +28,18 @@ void UBossHUDWidget::InitBossHUD(const FString& BossName, float MaxHP)
 
 void UBossHUDWidget::UpdateHP(float NewHP)
 {
-	if (!BossHPBar || MaxBossHP <= 0.f) return;
+	if (MaxBossHP <= 0.f) return;
 
-	float NormalizedHP = FMath::Clamp(NewHP / MaxBossHP, 0.f, 1.f);
-	BossHPBar->SetPercent(NormalizedHP);
+	float Ratio = FMath::Clamp(NewHP / MaxBossHP, 0.f, 1.f);
+
+	if (BossHPBar)
+	{
+		BossHPBar->SetPercent(Ratio);
+	}
+
+	if (BossHPText)
+	{
+		FString Text = FString::Printf(TEXT("%.0f"), NewHP);
+		BossHPText->SetText(FText::FromString(Text));
+	}
 }
