70ce510d07e310ee7eec21a34085791db22acead
Cheon <j1219j1@naver.com>
2025-06-13 18:06:37 +0900

    [add] : Chest, Mimic

---
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/Monster/NormalMonster/Mimic/DW_Mimic.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster/NormalMonster/Mimic/DW_Mimic.h"
+
+
+// Sets default values
+ADW_Mimic::ADW_Mimic()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ADW_Mimic::Interact_Implementation(AActor* Interactor)
+{
+	UE_LOG(LogTemp, Display, TEXT("Interacting %s"), *GetName());
+}
