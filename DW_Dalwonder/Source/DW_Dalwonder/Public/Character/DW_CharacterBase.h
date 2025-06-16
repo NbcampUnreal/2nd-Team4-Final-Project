974978c86b58100c4027665270638b0c02350746
Menho91 <agh914@naver.com>
2025-06-16 19:19:00 +0900

    [fix] Fixed Camera Check Logic Location

--- DW_Dalwonder/Source/DW_Dalwonder/Public/Character/DW_CharacterBase.h
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/Character/DW_CharacterBase.h
@@ -81,6 +81,7 @@ public:
 	
 	AActor* GetWeapon() const { return Weapon->GetChildActor(); }
 
+	UFUNCTION(BlueprintCallable)
 	virtual void SetWeaponType(int32 NewWeaponType);
 
 	UCharacterStatComponent* GetCharacterStatComponent() const { return StatComponent; }
