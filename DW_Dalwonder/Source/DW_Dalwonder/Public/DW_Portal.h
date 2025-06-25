1dff840f2a7983f24acfc9535e9a07850ee8bcea
nyamnyumnyeom <jincar000@naver.com>
2025-06-25 23:58:49 +0900

    [ Add ] New Sword

--- DW_Dalwonder/Source/DW_Dalwonder/Public/DW_Portal.h
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/DW_Portal.h
@@ -1,22 +1,22 @@
-#pragma once
-
-#include "CoreMinimal.h"
-#include "DW_InteractItemBase.h"
-#include "GameFramework/Actor.h"
-#include "DW_Portal.generated.h"
-
-UCLASS()
-class DW_DALWONDER_API ADW_Portal : public ADW_InteractItemBase
-{
-	GENERATED_BODY()
-	
-public:	
-	ADW_Portal();
-
-protected:
-
-
-public:
-	virtual void Interact_Implementation(AActor* Interactor) override;
-
-};
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "DW_InteractItemBase.h"
+//#include "GameFramework/Actor.h"
+//#include "DW_Portal.generated.h"
+//
+//UCLASS()
+//class DW_DALWONDER_API ADW_Portal : public ADW_InteractItemBase
+//{
+//	GENERATED_BODY()
+//	
+//public:	
+//	ADW_Portal();
+//
+//protected:
+//
+//
+//public:
+//	virtual void Interact_Implementation(AActor* Interactor) override;
+//
+//};
