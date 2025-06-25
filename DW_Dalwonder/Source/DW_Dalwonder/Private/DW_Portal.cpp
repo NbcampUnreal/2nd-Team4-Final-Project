0253b8d0625ff58b764aad1ab93d5f26c0b952bb
nyamnyumnyeom <jincar000@naver.com>
2025-06-25 22:04:43 +0900

    [ add ] Portal

--- DW_Dalwonder/Source/DW_Dalwonder/Private/DW_Portal.cpp
+++ DW_Dalwonder/Source/DW_Dalwonder/Private/DW_Portal.cpp
@@ -1,27 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "DW_Portal.h"
+#include "DW_GmBase.h"
 
-// Sets default values
-ADW_Portal::ADW_Portal()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
-}
-
-// Called when the game starts or when spawned
 void ADW_Portal::BeginPlay()
 {
-	Super::BeginPlay();
 	
 }
 
-// Called every frame
-void ADW_Portal::Tick(float DeltaTime)
+void ADW_Portal::Interact_Implementation(AActor* Interactor)
 {
-	Super::Tick(DeltaTime);
+    Super::Interact_Implementation(Interactor);
 
 }
 
--- DW_Dalwonder/Source/DW_Dalwonder/Public/DW_Portal.h
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/DW_Portal.h
@@ -1,26 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DW_InteractItemBase.h"
 #include "GameFramework/Actor.h"
 #include "DW_Portal.generated.h"
 
 UCLASS()
-class DW_DALWONDER_API ADW_Portal : public AActor
+class DW_DALWONDER_API ADW_Portal : public ADW_InteractItemBase
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	ADW_Portal();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+
+public:
+	virtual void Interact_Implementation(AActor* Interactor) override;
 
 };
