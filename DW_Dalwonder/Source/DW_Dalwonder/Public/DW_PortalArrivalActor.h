234f3c1507b98e3c25514c8143ceca8adbee4d6a
Cheon <j1219j1@naver.com>
2025-06-26 05:00:42 +0900

    [Add] : PortalArrivalActor
    포탈 도착 액터 추가
    
    포탈<->포탈이 아닌
    포탈->포탈도착액터 구조

---
+++ DW_Dalwonder/Source/DW_Dalwonder/Public/DW_PortalArrivalActor.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DW_Portal.h"
+#include "NiagaraSystem.h"
+#include "GameFramework/Actor.h"
+#include "DW_PortalArrivalActor.generated.h"
+
+UCLASS()
+class DW_DALWONDER_API ADW_PortalArrivalActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADW_PortalArrivalActor();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Portal")
+	EPortalType PortalType = EPortalType::Portal1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Portal")
+	UNiagaraSystem* SpawnNS = nullptr;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
